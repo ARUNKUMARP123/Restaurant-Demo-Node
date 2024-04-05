a316d845e8c958651911bbdbeb3a86908a9b7d06
ARUNKUMARP123 <arun841988@gmail.com>
2024-04-05 20:26:46 +0530

    base commit node

---
+++ index.jsx
@@ -0,0 +1,26 @@
+const express=require("express");
+
+
+const app=express();
+
+const bodyparser=require("body-parser");
+const cors=require("cors");
+
+
+app.use(cors());
+app.use(bodyparser.json( ));
+app.get("/",(req,res,next)=>{
+  res.send("Server Working");
+})
+
+app.post("/login",(req,res,next)=>{
+console.log(req.body);
+})
+
+app.post("/registration",(req,res,next)=>{
+    console.log(req.body);
+    })
+
+app.listen(4000,()=>{
+    console.log("Server Started at 4000")
+})
